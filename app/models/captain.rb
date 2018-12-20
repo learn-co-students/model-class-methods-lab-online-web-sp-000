cf77e9176fe73bdfaeb0a2b75f08b38e5e123fca
wingedmachine <wheeler.pryor@gmail.com>
2018-12-20 15:30:53 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: [:classifications])
+      .where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.joins(boats: [:classifications])
+      .where("classifications.name = 'Sailboat'")
+      .group(:name)
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: :classifications)
+      .where("classifications.name IN ('Motorboat', 'Sailboat')")
+      .group('captains.name')
+      .having('COUNT(DISTINCT(classifications.name)) = 2')
+  end
+
+  def self.non_sailors
+    self.where("id NOT IN (?)", self.sailors.pluck(:id))
+  end
 end
