7a9cbcb682beacf558d4e253c39ffbf85b4854ef
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-03 13:51:52 -0500

    deleted old method

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,23 +1,6 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def self._catamaran_operators  # => <Captain::ActiveRecord_Relation>
-
-    # => Boats with class Catamaran. <Boat::ActiveRecord_Relation>
-    catamarans = Boat.joins(:classifications)   
-      .where("classifications.name=?","Catamaran")
-    catamarans.map {|c| c.captain} # => Correct data, wrong type (array)
-
-    captains_with_boats = Captain.joins(:boats) # => <Captain::ActiveRecord_Relation>
-      .joins(:classifications) # empty Captain::ActiveRecord_Relation
-      .where('classifications.name=?', 'Catamaran')
-
-    # This doesn't work in Ruby but its generated SQL works in SQL. But, the generated SQL says nothing about classifications so never mind.
-    captains_with_boats = Captain.joins(:boats).includes(:classifications)
-    
-    Captain.joins(:boats).includes(:classifications).where('classifications.name=?', 'Catamaran').references(:classifications)
-  end
-
   def self.catamaran_operators  # => <Captain::ActiveRecord_Relation>
     # Captain.joins(:boat).where(boats: {classification_id: cat_class.id}) # empty
     
@@ -43,6 +26,7 @@ class Captain < ActiveRecord::Base
   
   # returns people who are not captains of sailboats
   def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
 end
