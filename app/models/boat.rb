16e8c0168d9e26259806852b02f242e070354ccf
aburk3 <aburk3@gmail.com>
2019-02-04 09:59:09 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,9 +24,14 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    includeds(:classifications).where(:name => "Sailboats")
+    includes(:classifications).where(classifications: { :name => "Sailboat" })
   end
 
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3")
+  end
 
-
+  def self.longest
+    order('length DESC').first
+  end
 end
