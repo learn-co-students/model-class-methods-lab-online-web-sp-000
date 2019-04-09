ba4100096a48dbcf20cb66a4048429ef0da36e22
Anup Khadka <akhadka.dev@gmail.com>
2019-04-09 21:10:27 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: [:classifications]).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    self.joins(boats: [:classifications]).where("classifications.name = ?", "Sailboat").distinct
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: [:classifications]).where("classifications.name IN ('Sailboat', 'Motorboat')").group("captains.id").having("count(DISTINCT classifications.name) >= ?", 2)
+  end
+
+  def self.non_sailors
+    self.joins(boats: [:classifications]).where("classifications.name != ?", "Sailboat")
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    maximum_length = Boat.maximum('length')
+    self.joins(:boats).where('boats.length = ?', maximum_length)
+  end
+
 end
