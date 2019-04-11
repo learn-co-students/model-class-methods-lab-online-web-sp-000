66832e6db449bfa310eb6659d9f60ff8a5773db5
github username <github email address>
2019-04-11 14:13:20 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,6 +5,9 @@ class Captain < ActiveRecord::Base
       joins(boats: [:classifications]).where(classifications: {name: "Catamaran"}).uniq
   end
 
+  def self.motorboat
+     joins(boats: :classifications).where(classifications: { name: "Motorboat"} ).uniq
+  end
 
   def self.sailors
     joins(boats: [:classifications]).where(classifications: {name: "Sailboat"}).uniq
@@ -12,8 +15,8 @@ class Captain < ActiveRecord::Base
 
 
   def self.talented_seafarers
-      self.joins(boats: [:classifications]).where("classifications.name IN ('Sailboat', 'Motorboat')").group("captains.id").having("count(DISTINCT classifications.name) >= ?", 2)
-    end
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat.pluck(:id))
+  end
 
   def self.non_sailors
     where("id NOT IN (?)", self.sailors.pluck(:id))
