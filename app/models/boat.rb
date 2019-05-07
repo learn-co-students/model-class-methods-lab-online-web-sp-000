09fc4f254c600d9b407829d446ec74db448bc993
Tyler Hunt <tyhunt04@gmail.com>
2019-05-07 17:04:21 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,32 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    take(5)
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    order(:name).last(3).reverse
+  end
+
+  def self.without_a_captain
+    includes(:captain).where(captain: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
 end
--- spec/models/captain_spec.rb
+++ spec/models/captain_spec.rb
@@ -17,7 +17,7 @@ describe Captain do
 
   describe "::talented_seafarers" do
     it "returns captains of motorboats and sailboats" do
-      captains = ["Captain Cook", "Samuel Axe"]
+      captains = ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
       expect(Captain.talented_seafarers.pluck(:name)).to eq(captains)
     end
   end
