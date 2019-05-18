2f4ea8afd9003ec9045be59e66c5a94a303d058d
Btate712 <btate712@gmail.com>
2019-05-18 06:58:51 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.select { |captain| captain.boats.any? { |boat| Boat.catamarans.include?(boat) } }
+  end
+
+  def self.sailors
+    Captain.select { |captain| captain.boats.any? { |boat| Boat.sailboats.include?(boat) } }
+  end
+
+  def self.motorboat_operators
+    Captain.select { |captain| captain.boats.any? { |boat| Boat.motorboats.include?(boat) } }
+  end
+
+  def self.talented_seafarers
+    Captain.select do |captain|
+      Captain.sailors.include?(captain) && Captain.motorboat_operators.include?(captain)
+    end
+  end
+
+  def self.non_sailors
+    Captain.select { |captain| !Captain.sailors.include?(captain) }
+  end
 end
