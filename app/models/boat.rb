07f487f9fc959702aa0c5280b1689c627976a790
Diana Badas <diana.badas@gmail.com>
2019-06-07 15:00:02 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -26,4 +26,12 @@ class Boat < ActiveRecord::Base
   def self.sailboats
     includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
+
+  def self.longest
+    order("length DESC").first
+  end
 end
